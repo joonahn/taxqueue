501aea8ccf30537f58be21de1d8bd2afaa3e6713
joonahn <sdeveloper@naver.com>
2017-06-20 11:58:26 +0900

    uclust and blast now use rdpdb parameter

--- script/taxassn.sh
+++ script/taxassn.sh
@@ -20,14 +20,6 @@ echo -e "${RED}activate succeeded${NC}"
 # taxonomy assign
 if [[ $taxalg == *"RDP"* ]]; then
 	export RDP_JAR_PATH="/home/qiime/app/rdp_classifier_2.2/rdp_classifier-2.2.jar"
-	
-	# if [[ $rdpdb == *"greengenes"* ]]; then
-	# 	# tax assign with greengenes db
-	# 	assign_taxonomy.py -i "${ffolder}/otus.fa" \
-	# 	 -t "./gg_otus_4feb2011/taxonomies/greengenes_tax.txt" \
-	# 	 -r "./gg_otus_4feb2011/rep_set/gg_97_otus_4feb2011.fasta" \
-	# 	 -c "${conflevel}" \
-	# 	 -o "${ffolder}/tax_output" -m rdp	
 
 	if [[ $rdpdb == *"greengenes"* ]]; then
 		# tax assign with greengenes db
@@ -55,14 +47,54 @@ if [[ $taxalg == *"RDP"* ]]; then
 	fi
 
 elif [[ $taxalg == *"BLAST"* ]]; then
+
 	# tax assign with BLAST
-	parallel_assign_taxonomy_blast.py -i "${ffolder}/otus.fa" \
-	 -o "${ffolder}/tax_output"
+	if [[ $rdpdb == *"greengenes"* ]]; then
+		# tax assign with greengenes db
+		parallel_assign_taxonomy_blast.py -i "${ffolder}/otus.fa" \
+		 -t "./gg_otus_may2013/taxonomy/97_otu_taxonomy.txt" \
+		 -r "./gg_otus_may2013/rep_set/97_otus.fasta" \
+		 -o "${ffolder}/tax_output"
+
+	elif [[ $rdpdb == *"silva"* ]]; then
+		# tax assign with silva db
+		parallel_assign_taxonomy_blast.py -i "${ffolder}/otus.fa" \
+		 -t "./SILVA_128_QIIME_release/taxonomy/16S_only/97/consensus_taxonomy_7_levels.txt" \
+		 -r "./SILVA_128_QIIME_release/rep_set/rep_set_16S_only/97/97_otus_16S.fasta" \
+		 -o "${ffolder}/tax_output"
+
+	elif [[ $rdpdb == *"unite"* ]]; then
+			#statements
+			echo "ERROR: Unimplemented"
+	else
+		# Error
+		echo "ERROR: RDP method is not specified"
+	fi
 
 elif [[ $taxalg == *"UCLUST"* ]]; then
-	# tax assign with BLAST
-	assign_taxonomy.py -i "${ffolder}/otus.fa" \
-	 -o "${ffolder}/tax_output" -m uclust
+
+	# tax assign with UCLUST
+	if [[ $rdpdb == *"greengenes"* ]]; then
+		# tax assign with greengenes db
+		assign_taxonomy.py -i "${ffolder}/otus.fa" \
+		 -t "./gg_otus_may2013/taxonomy/97_otu_taxonomy.txt" \
+		 -r "./gg_otus_may2013/rep_set/97_otus.fasta" \
+		 -o "${ffolder}/tax_output" -m uclust
+
+	elif [[ $rdpdb == *"silva"* ]]; then
+		# tax assign with silva db
+		assign_taxonomy.py -i "${ffolder}/otus.fa" \
+		 -t "./SILVA_128_QIIME_release/taxonomy/16S_only/97/consensus_taxonomy_7_levels.txt" \
+		 -r "./SILVA_128_QIIME_release/rep_set/rep_set_16S_only/97/97_otus_16S.fasta" \
+		 -o "${ffolder}/tax_output" -m uclust
+
+	elif [[ $rdpdb == *"unite"* ]]; then
+			#statements
+			echo "ERROR: Unimplemented"
+	else
+		# Error
+		echo "ERROR: RDP method is not specified"
+	fi
 
 else
 	echo "ERROR: tax assign algorithm is not specified"
